09d9ee4c867deac8baba8b20c8e1562f709d7702
Radha Kandula <rkandula@ufl.edu>
2014-09-30 12:31:17 -0400

    add fields() function to Event class to get all the fields of it

--- bin/form.py
+++ bin/form.py
@@ -40,6 +40,9 @@ class Event(object):
         else:
             raise Exception("Malformed XML: multiple fields with the name {0}".
                             format(name))
+    def fields(self):
+        for node in self._node.xpath("./field[.]"):
+            yield Field(node)
 
     @property
     def name(self):
