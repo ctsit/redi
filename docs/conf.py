3bfdc4fdcc98445e8e18954c72655acc7d02b916
Andrei Sura <indera@gmail.com>
2014-10-02 18:01:45 -0400

    Use default theme in `docs/conf.py`

--- docs/conf.py
+++ docs/conf.py
@@ -109,7 +109,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-#html_theme = 'default'
+html_theme = 'default'
 #html_theme = 'nature'
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
