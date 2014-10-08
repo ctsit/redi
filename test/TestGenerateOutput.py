111d4f2e8ca6b2f79421cb14daaa69c4e3207d26
Andrei Sura <indera@gmail.com>
2014-10-08 14:28:04 -0400

    This reverts commit 6947fe2edd806342efc59d602f4a3325525ed2cf. Because we want to keep the code.

--- test/TestGenerateOutput.py
+++ test/TestGenerateOutput.py
@@ -37,7 +37,7 @@ class TestGenerateOutput(unittest.TestCase):
             + "#test_person_form_event() using xml: " )
         string_1_xml = """
 <person_form_event>
-    <person>
+    <person lab_id="999-0100">
         <study_id>100</study_id>
         <all_form_events>
             <form>
@@ -63,7 +63,7 @@ class TestGenerateOutput(unittest.TestCase):
  
         </all_form_events>
     </person>
-    <person>
+    <person lab_id="999-0099">
         <study_id>99</study_id>
         <all_form_events>
             <form>
@@ -112,7 +112,7 @@ class TestGenerateOutput(unittest.TestCase):
             </form>
         </all_form_events>
     </person>
-    <person>
+    <person lab_id="999-0098">
         <study_id>98</study_id>
         <all_form_events>
             <form>
@@ -144,9 +144,9 @@ class TestGenerateOutput(unittest.TestCase):
 
         form_details    = {'Total_cbc_Forms': 2, 'Total_inr_Forms': 3}
         subject_details = {
-            '98'  : {'Total_cbc_Forms' : 0, 'Total_inr_Forms' : 1 },
-            '99'  : {'Total_cbc_Forms' : 1, 'Total_inr_Forms' : 1 },
-            '100' : {'Total_cbc_Forms' : 1, 'Total_inr_Forms' : 1 }
+                '98'  : {'Total_cbc_Forms' : 0, 'Total_inr_Forms' : 1, 'lab_id': "999-0098" },
+                '99'  : {'Total_cbc_Forms' : 1, 'Total_inr_Forms' : 1, "lab_id": "999-0099" },
+                '100' : {'Total_cbc_Forms' : 1, 'Total_inr_Forms' : 1, "lab_id": "999-0100" }
         } 
  
         report_data = {
