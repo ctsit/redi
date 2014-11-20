a0834d429440962512f3b029ea78658cd64ff6a8
Taeber Rapczak <taeber@ufl.edu>
2014-11-20 13:29:19 -0500

    Update version number to 0.13.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='redi-py',
-    version='0.12.0',
+    version='0.13.0',
     author='https://www.ctsi.ufl.edu/research/study-development/informatics-consulting/',
     author_email='cts-it-red@ctsi.ufl.edu',
     packages=find_packages(exclude=['test']),
@@ -28,7 +28,7 @@ setup(
         'redi': ['README.md'],
     },
     url='https://github.com/ctsit/redi',
-    download_url = 'https://github.com/ctsit/redi/releases/tag/0.12.0',
+    download_url = 'https://github.com/ctsit/redi/releases/tag/0.13.0',
     keywords = ['EMR', 'EHR', 'REDCap', 'Clinical Data'],
     license='BSD 3-Clause',
     description='REDCap Electronic Data Importer',
@@ -48,6 +48,7 @@ setup(
     test_suite='test.TestSuite',
     tests_require=[
         "mock >= 1.0",
+        "sftpserver >= 0.2",
     ],
     setup_requires=[
         "nose >= 1.0",
