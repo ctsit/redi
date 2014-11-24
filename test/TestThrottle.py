1335a77bac29d9aabc77b693901288d5697c64c0
Taeber Rapczak <taeber@ufl.edu>
2014-11-24 16:52:11 -0500

    Fix a big flaw in the Throttle algorithm
    Mohan discovered during his testing that the new throttling algorithm
    was not working and that my new code was causing our IP to be banned. I
    found that the code was fundamentally flawed and wrote some additional
    unit tests to make sure it works correctly.
    
    Basically, by clearing the deque after sleeping, I was essentially
    assuming--quite wrongly--that all timestamps were at least as old as the first
    one. This exact opposite of which is true.
    
    Now, _wait() will only remove old entries from the deque, instead of all
    of them, once the rate limit is hit.
    
    Note: users should still probably set their rate limits to a few less
    than the REDCap Rate Limit (default=600) to compensate for other web
    requests which count against them in addition to RED-I's API calls. A
    known example is PyCap's API calls when instantiating a Project object.

--- test/TestThrottle.py
+++ test/TestThrottle.py
@@ -9,34 +9,76 @@ from redi.utils import throttle
 class TestThrottle(unittest.TestCase):
 
     def test_throttle(self):
-        class Clock(object):
-            def __init__(self):
-                self.now = datetime.datetime.now()
-
-            def __call__(self):
-                return self.now
-
-            def add_seconds(self, seconds):
-                self.now += datetime.timedelta(seconds=seconds)
-
-        clock = Clock()
+        clock = MockClock()
         throttle.Throttle._now = clock
         throttle.Throttle._sleep = clock.add_seconds
 
         call = throttle.Throttle(lambda: None, max_calls=3,
                                  interval_in_seconds=5)
 
-        call()  # t=0
-        clock.add_seconds(1)
-        call()  # t=1
-        clock.add_seconds(2)
-        call()  # t=3
-        clock.add_seconds(1)
-        call()  # t=4
+        call()
+        call()
+        call()
+        self.assertEquals(3, len(call._timestamps))
+        before = clock.now
+
+        call()
+
+        self.assertEqual(5, (clock.now - before).total_seconds())
+
+    def test_throttle_front_loaded(self):
+        clock = MockClock()
+        throttle.Throttle._now = clock
+        throttle.Throttle._sleep = clock.add_seconds
+
+        call = throttle.Throttle(lambda: None, max_calls=3,
+                                 interval_in_seconds=5)
+
+        call()
+        call()
+        call()
+        self.assertEquals(3, len(call._timestamps))
+        clock.add_seconds(5)
+        before = clock.now
+
+        call()
+
         self.assertEquals(1, len(call._timestamps))
-        clock.add_seconds(1)
-        call()  # t=5
+        self.assertEqual(0, (clock.now - before).total_seconds())
+
+    def test_throttle_back_loaded(self):
+        clock = MockClock()
+        throttle.Throttle._now = clock
+        throttle.Throttle._sleep = clock.add_seconds
+
+        call = throttle.Throttle(lambda: None, max_calls=3,
+                                 interval_in_seconds=5)
+
+        start = clock.now
+        call()
+        clock.add_seconds(4)
+        call()
+        call()
+        self.assertEquals(3, len(call._timestamps))
+
+        call()
+        self.assertEquals(3, len(call._timestamps))
+        self.assertEqual(5, (clock.now - start).total_seconds())
+
+        call()
         self.assertEquals(2, len(call._timestamps))
+        self.assertEqual(9, (clock.now - start).total_seconds())
+
+
+class MockClock(object):
+    def __init__(self):
+        self.now = datetime.datetime.now()
+
+    def __call__(self):
+        return self.now
+
+    def add_seconds(self, seconds):
+        self.now += datetime.timedelta(seconds=seconds)
 
 
 if __name__ == '__main__':
