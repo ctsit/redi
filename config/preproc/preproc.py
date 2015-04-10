c4007f8bad50a927bd7c47a120ab0fe6e8e6fa2d
Taeber Rapczak <taeber@ufl.edu>
2015-04-10 11:11:44 -0400

    Stub out baseline-2 filtering

--- config/preproc/preproc.py
+++ config/preproc/preproc.py
@@ -8,13 +8,34 @@
 #
 # Distributed under the BSD 3-Clause License
 # For full text of the BSD 3-Clause License see http://opensource.org/licenses/BSD-3-Clause
+import csv
+
+
 def main():
     run_processing()
 
-def run_processing():
-    print "Hi I am the preprocessing file yo! Drop it like its a hot tater"
 
-# todo create example of where this returns 0
+def run_processing():
+    backup('raw.txt')
+    rows = csv.DictReader('raw.txt')
+    subject_ids = list()
+    for row in rows:
+        subject_ids.append(row['STUDY_ID'])
+
+    consent_dates = fetch_consent_dates(mrns)
+    panels = fetch_panels('clinical-componenet-to-loinc.xml', 'translationTable.xml')
+    # panels = {
+    #     'rna': [1230],
+    #     'cbc': [600, 712, 372]
+    # }
+    grouped_by_panel = group_rows_by_panel(panels, rows)
+    # grouped_by_panel = {
+    #     'rna': [<csv_row>, <csv_row>, <csv_row>],
+    #     'cbc': [],
+    #     'NONE': [<csv_row>, <csv_row>]
+    # }
+    save(filter_old_labs(grouped_records, consent_dates))
+
 
 if __name__ == "__main__":
     main()
